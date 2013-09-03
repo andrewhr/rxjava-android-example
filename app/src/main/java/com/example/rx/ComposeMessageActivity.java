3627a39bd9b637e0a0a3b60c266394f8787d19e4
Andrew Rosa <andrewhr@me.com>
2013-09-03 00:01:05 -0300

    Application setup and message composition layout
    Just a basic application setup. Also, includes a "Message composition"
    layout to serve as a runnable app mockup.

---
+++ app/src/main/java/com/example/rx/ComposeMessageActivity.java
@@ -0,0 +1,12 @@
+package com.example.rx;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ComposeMessageActivity extends Activity {
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.compose_message);
+    }
+}
